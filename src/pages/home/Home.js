54fdb86ff70c88965a5f5e35ba1076b6059990c1
Kranium2002 <vidhumathur2002@gmail.com>
2022-09-18 15:57:59 +0530

    Added css for news and notices

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -27,8 +27,40 @@ const Home = () => {
       <NavBar></NavBar>
       <h1 className="heading2">Home</h1>
       <div className="landing-flex-home">
-        <div className="left-home">left</div>
-        <div className="right-home">right</div>
+        <div className="left-home">
+          <div className='home-heading'>
+            Latest News
+          </div>
+          <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/news/result-of-upgradation-2022-23_OEz7enX.pdf'>Result of Upgradation 2022-23</a>
+          </div>
+          <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/news/nsp-portal-under-three-central-sector-scholarship-schemes-for-minority-community-st_VNtMBc9.pdf'>NSP portal under three Central Sector Scholarship Schemes for Minority Community Students for Academic Year 2022-23</a>
+          </div>
+          <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/news/nsp-portal-under-three-central-sector-scholarship-schemes-for-minority-community-st_u1VbVfT.pdf'> NSP portal under three Central Sector Scholarship Schemes for Minority Community Students for Academic Year 2022-23</a>
+          </div>
+          <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/news/registration-form-of-hostel-for-1st-year-students.pdf'>Registration form of hostel for 1st year students</a>
+          </div>
+          </div>
+        <div className="right-home">
+          <div className='home-heading'>
+            Notices
+            </div>
+            <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/news/admission-notice-academic-session-2022-23_C9GHkDO.pdf'>Admission Notice Academic Session 2022-23</a>
+          </div>
+          <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/notices/time-table-_sep-dec_2022.pdf'>Time Table Sep-Dec 2022</a>
+          </div>
+          <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/notices/participation-in-national-research-paper-writing-competition-under-the-theme-sub_uXRz2B3.pdf'>Participation in National Research Paper Writing Competition under the theme "Subhash-Swaraj-Sarkar"</a>
+          </div>
+          <div className='home-element'>
+            <a target="_blank" rel="noopener noreferrer" href='https://www.msit.in/media/notices/pragati-and-saksham-scholarship-scheme.pdf'>Pragati and Saksham scholarship scheme</a>
+          </div>
+            </div>
         
       </div>
       <button onClick={handleNewGroup} className="landing-button margin">
